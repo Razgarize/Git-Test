80bbb12fc87a658723e24248907237c70c11aa33
James Hart <jhart44@ut.utm.edu>
2024-01-12 12:32:36 -0600

    Changed to make the sum from 1 to input number.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+int sum(int n)
+{
+  int sum = 0;
+  for(int i = 1; i <= n; i++)
+    {
+      sum += i;
+    }
+  return sum;
+}
+
 int main()
 {
-  std::cout << "Hello World!";
+  //setup
+  int n = 0;
+  //input
+  std::cout << "input a integer: ";
+  std::cin >> n;
+  //process
+  std::cout << sum(n);
+  
   
   return 0;
 }
